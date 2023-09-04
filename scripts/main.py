388b726b062bf553428f4d53d31f10f1568ca36c
KareemElozeiri <k.elozeiri@gmail.com>
2023-09-04 20:17:36 +0300

    feat(configs): add path configuration for loading data

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,17 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+module_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(module_dir)
+
+'''script start here'''
+from cqiforecasting.configs.config import CFGLog
+from cqiforecasting.dataloader.nn_data_loader import NNDataLoader
+
+
+
 
 if __name__ == "__main__":
-    pass 
+    df = NNDataLoader().load_data(CFGLog["data"])
+    print(df.head())
