1eaaac2ba9031ccbdf78510804858e851950d2d8
KareemElozeiri <k.elozeiri@gmail.com>
2023-09-19 22:03:45 +0300

    chore(NNDataloader): refactoring to apply SOLID

--- cqiforecasting/dataloader/nn_data_loader.py
+++ cqiforecasting/dataloader/nn_data_loader.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-class NNDataLoader:
-    
+class DataLoader:
     def __init__(self, data_config) -> None:
         self._data_config = data_config
     
@@ -11,9 +10,11 @@ class NNDataLoader:
         df = pd.read_csv(self._data_config["path"])
         return df
 
+
+class SequenceSplitter:
     def split_sequences(self, df, seq_length):
         X, y = [], []
-        for i in range(len(df)-seq_length):
+        for i in range(len(df) - seq_length):
             X.append(df[i:i+seq_length])
             y.append(df[i+seq_length])
             
@@ -21,24 +22,34 @@ class NNDataLoader:
         y = np.array(y)
         return X, y
 
-    def preprocess_data(self, dataset, seq_length):
 
-        train_size = int(self._data_config["train_size"]*len(dataset))
-        validation_size = int(self._data_config["validation_size"]*len(dataset))  
-        #test_size = int(self._data_config["test_size"]*len(dataset))
+class DataPreprocessor:
+    def __init__(self, data_config) -> None:
+        self._data_config = data_config
+    
+    def preprocess_data(self, dataset, seq_length):
+        train_size = int(self._data_config["train_size"] * len(dataset))
+        validation_size = int(self._data_config["validation_size"] * len(dataset))
+        # test_size = int(self._data_config["test_size"]*len(dataset))
 
         train_data = dataset[:train_size]
         val_data = dataset[train_size:validation_size]
         test_data = dataset[train_size+validation_size:]
 
-        X_train, y_train = self.split_sequences(train_data, seq_length)
-        X_val, y_val = self.split_sequences(val_data, seq_length)
-        X_test, y_test = self.split_sequences(test_data, seq_length)
+        return train_data, val_data, test_data
 
-        return X_train, X_val, X_test, y_train, y_val, y_test
+
+class NNDataLoader:
+    def __init__(self, data_config) -> None:
+        self._data_config = data_config
+        self._data_loader = DataLoader(data_config)
+        self._sequence_splitter = SequenceSplitter()
+        self._data_preprocessor = DataPreprocessor(data_config)
     
     def load_sequences(self, seq_length):
-        df = self.load_data()
-
-        return self.preprocess_data(df, seq_length)
-
+        df = self._data_loader.load_data()
+        train_data, val_data, test_data = self._data_preprocessor.preprocess_data(df, seq_length)
+        X_train, y_train = self._sequence_splitter.split_sequences(train_data, seq_length)
+        X_val, y_val = self._sequence_splitter.split_sequences(val_data, seq_length)
+        X_test, y_test = self._sequence_splitter.split_sequences(test_data, seq_length)
+        return X_train, X_val, X_test, y_train, y_val, y_test
---
+++ cqiforecasting/executor/inferer.py
@@ -0,0 +1,11 @@
+
+class Inferer:
+
+    def __init__(self) -> None:
+        pass
+
+    def load_model(self):
+        pass 
+
+    def infer(self, input_sequence, steps_num):
+        pass
