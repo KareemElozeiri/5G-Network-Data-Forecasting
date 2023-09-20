bae8d7d1b66cdddb076cc0bb3af87cae09fa0b10
KareemElozeiri <k.elozeiri@gmail.com>
2023-09-20 04:30:10 +0300

    feat(Inferer): add Inferer functions

--- cqiforecasting/executor/inferer.py
+++ cqiforecasting/executor/inferer.py
@@ -1,11 +1,24 @@
+import numpy as np 
 
 class Inferer:
+    @staticmethod
+    def infer(model, input_sequences) -> np.array:
+        output = model.predict(input_sequences)
+        return output
+    
+    @staticmethod
+    def infer_steps(model, input_sequence, num_steps=1) -> np.array:
+        x_forecast = np.array([input_sequence])
+        y_forecast = np.array([[0],])
 
-    def __init__(self) -> None:
-        pass
+        for _ in range(num_steps):
+            p = model.predict(np.array([x_forecast[-1],]))
+            y_forecast = np.append(y_forecast, p, axis=0)
+            new_arr = x_forecast[-1].copy()
+            new_arr[:-1] = new_arr[1:]
+            new_arr[-1, :] = y_forecast[-1]
+            x_forecast = np.append(x_forecast, [new_arr,], axis=0)
 
-    def load_model(self):
-        pass 
+        y_forecast = y_forecast[1:]
 
-    def infer(self, input_sequence, steps_num):
-        pass
+        return y_forecast
